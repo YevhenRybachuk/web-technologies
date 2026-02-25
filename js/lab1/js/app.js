29a4840cf4e57f94988c0581ea41a6269fa1d065
Yevhen <evgenribacuk@gmail.com>
2026-02-25 22:08:18 +0200

    lab1 complete

---
+++ js/lab1/js/app.js
@@ -0,0 +1,8 @@
+const heading = document.getElementById("title");
+heading.textContent = "Hello world!";
+
+const button = document.getElementById("btn");
+
+button.onclick = function () {
+  console.log("Рибачук Євген");
+};
